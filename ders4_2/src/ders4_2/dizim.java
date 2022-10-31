58a0db17c3ae6939914ae99b9b56319ac89c2e3a
DELL <DELL@10.48.158.60>
2022-10-31 11:30:52 +0300

    1

---
+++ ders4_2/src/ders4_2/dizim.java
@@ -0,0 +1,28 @@
+package ders4_2;
+
+import java.util.ArrayList;
+
+public class dizim {
+
+	public static void main(String[] args) {
+		ArrayList<String> item =new ArrayList<String>();
+		item.add("red");
+		item.add("yellow");
+		item.add(0,"green");
+        for(String renk:item) {
+        	System.out.print(" " +renk);
+        	
+        	
+        }
+        item.remove("red");
+        System.out.println("");
+        for(String renk:item) {
+        	System.out.print(" " +renk);
+
+}
+        System.out.println();
+        System.out.print(item.get(1));
+        System.out.println(item.contains("red")  ? "? \nevet var": "\nhayır yok");
+
+}
+}
